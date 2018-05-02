cf055cb8a6a8d8efc4273297fa1c4f08481b7990
Floren HENRI <fhenri@e1r7p8.42.fr>
2018-05-02 10:43:34 +0200

    [UPD] update webapack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,55 @@
-var path = require('path');
-
+const path = require('path')
+const webpack = require('webpack')
 const src = path.resolve(__dirname, './src')
 
 module.exports = {
-  entry: ['babel-polyfill','./src/client/index.js'],
+  mode: 'development',
+
+  entry: ['babel-polyfill','./src/index.js'],
 
   output: {
-    path: path.join(__dirname, 'build'),
+    path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-
+  devServer: {
+    historyApiFallback: true,
+    disableHostCheck: true,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('development')
+      }
+    }),
+  ],
   module: {
-    loaders: [{
+    rules: [{
       test: /\.js$/,
       exclude: /node_modules/,
-      loader: 'babel',
-      query:{
-        presets: ["es2015", "react", "stage-0"]
+      use: [{ loader: 'babel-loader',
+      options:{
+        presets: ["es2015", "react", "stage-0"],
+        plugins: [
+         ['import', { libraryName: "antd", style: true }]
+       ]
       }
-    }, {
-      test: /\.scss$/,
-loaders: ['style-loader', 'css-loader', 'sass-loader', 'autoprefixer-loader'],
-include: src,
-
     }]
-  }
 
-};
+    },
+     {
+      test: /\.scss$/,
+      loaders: ['style-loader', 'css-loader', 'sass-loader'],
+      include: src,
+
+    },
+    {
+        test: /\.less$/,
+        use: ["style-loader", {loader: 'css-loader', options: {sourceMap: 1}}, {
+            loader: "less-loader",
+            options: {
+                javascriptEnabled: true
+            }}]
+    }
+  ]
+  }
+}
